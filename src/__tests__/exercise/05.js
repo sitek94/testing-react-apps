1071a2285104070ba42c46eafd63e606e45d7561
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-06-20 05:47:31 +0200

    ex 5.2 - test unhappy path

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -16,16 +16,9 @@ const buildLoginForm = build({
   },
 })
 
-// 🐨 get the server setup with an async function to handle the login POST request:
-// 💰 here's something to get you started
 const server = setupServer(...handlers)
 
-// you'll want to respond with an JSON object that has the username.
-// 📜 https://mswjs.io/
-
-// 🐨 before all the tests, start the server with `server.listen()`
 beforeAll(() => server.listen())
-// 🐨 after all the tests, stop the server with `server.close()`
 afterAll(() => server.close())
 
 test(`logging in displays the user's username`, async () => {
@@ -34,17 +27,33 @@ test(`logging in displays the user's username`, async () => {
 
   await userEvent.type(screen.getByLabelText(/username/i), username)
   await userEvent.type(screen.getByLabelText(/password/i), password)
-  // 🐨 uncomment this and you'll start making the request!
   await userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  // as soon as the user hits submit, we render a spinner to the screen. That
-  // spinner has an aria-label of "loading" for accessibility purposes, so
-  // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
-  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
   await waitForElementToBeRemoved(() => screen.getByLabelText('loading...'))
 
-  // once the login is successful, then the loading spinner disappears and
-  // we render the username.
-  // 🐨 assert that the username is on the screen
   expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+test(`shows error message when username is missing`, async () => {
+  render(<Login />)
+  const {password} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText('loading...'))
+
+  expect(screen.getByText('username required')).toBeInTheDocument()
+})
+
+test(`shows error message when password is missing`, async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText('loading...'))
+
+  expect(screen.getByText('password required')).toBeInTheDocument()
 })
