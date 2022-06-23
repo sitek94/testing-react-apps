054f9a23fc114f6c6dec55280b8a788051632b53
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-06-23 20:51:32 +0200

    ex 7.1 - test dark theme

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -19,4 +19,17 @@ test('renders with the light styles for the light theme', () => {
   `)
 })
 
+test('renders with the dark styles for the dark theme', () => {
+  render(
+    <ThemeProvider initialTheme="dark">
+      <EasyButton>Easy</EasyButton>
+    </ThemeProvider>,
+  )
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
+})
+
 /* eslint no-unused-vars:0 */
