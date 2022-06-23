0b8f9aea856e7fbba40428c7e5dbeb9de06c519c
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-06-23 21:02:26 +0200

    ex 7.3 - use app test utils

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,18 +2,8 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render as rtlRender, screen} from '@testing-library/react'
-import {ThemeProvider} from '../../components/theme'
+import {render, screen} from 'test/test-utils'
 import EasyButton from '../../components/easy-button'
-
-function render(ui, {theme = 'light', ...options} = {}) {
-  return rtlRender(ui, {
-    wrapper: ({children}) => (
-      <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
-    ),
-    ...options,
-  })
-}
 
 test('renders with the light styles for the light theme', () => {
   render(<EasyButton>Easy</EasyButton>, {theme: 'light'})
