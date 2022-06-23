8dfd8a2d3f2d4a6e487da88435b40c96ad735475
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-06-23 20:50:40 +0200

    ex 7 - add missing provider

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -7,16 +7,16 @@ import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+  render(
+    <ThemeProvider initialTheme="light">
+      <EasyButton>Easy</EasyButton>
+    </ThemeProvider>,
+  )
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
 })
 
 /* eslint no-unused-vars:0 */
